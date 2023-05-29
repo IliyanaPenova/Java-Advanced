4537d68c3c099fba059e7e4aa5683cab943fe8a5
Iliyana Penova <110258890+iliyana523@users.noreply.github.com>
2023-05-29 23:16:12 +0300

    SoftUni
    Education

---
+++ Streams,FilesAndDirectories/SumLines.java
@@ -0,0 +1,27 @@
+package StreamsFilesAndDirectories;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SumLines {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader
+                (".idea/resources/04. Java-Advanced-Files-and-Streams-Exercises-Resources/input.txt"));
+        String line= bufferedReader.readLine();
+
+        while(line !=null){
+            long sum=0;
+            char[] charactersFromLine=line.toCharArray();
+            for (char character : charactersFromLine) {
+                sum+=character;
+            }
+            System.out.println(sum);
+            line= bufferedReader.readLine();
+        }
+        bufferedReader.close();
+
+    }
+
+}
