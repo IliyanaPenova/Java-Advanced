5181a8476f763b51345b4436171b0a550360b298
Iliyana Penova <110258890+iliyana523@users.noreply.github.com>
2023-06-04 16:21:15 +0300

    SoftUni
    Education

---
+++ FunctionalProgramming/SumNumbers.java
@@ -0,0 +1,23 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        Function<int[],String> count=elements->"Count = "+elements.length;
+        Function<int[],String> sum=elements->"Sum = " +Arrays.stream(elements).sum();
+
+
+        int[]numbers=Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(count.apply(numbers));
+        System.out.println(sum.apply(numbers));
+
+
+    }
+}
