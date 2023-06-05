4c338b376ab078ad0bb49b74b90bfc62da39b361
Iliyana Penova <110258890+iliyana523@users.noreply.github.com>
2023-06-05 21:10:17 +0300

    SoftUni
    Education

---
+++ FunctionalProgramming/CountUpperCaseWord.java
@@ -0,0 +1,24 @@
+package FunctionalProgramming;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class CountUpperCaseWord {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String[] words=scanner.nextLine().split("\\s+");
+
+        Predicate<String> isStartingWithUpperCase= w->Character.isUpperCase(w.charAt(0));
+
+        ArrayDeque<String> queueOfWords=new ArrayDeque<>();
+
+        Arrays.stream(words).filter(isStartingWithUpperCase).forEach(queueOfWords::offer);
+
+        System.out.println(queueOfWords.size());
+        while(!queueOfWords.isEmpty()){
+            System.out.println(queueOfWords.poll());
+        }
+    }
+}
