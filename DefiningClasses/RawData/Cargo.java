fb14515469fbe159dad007df79944ff65c76d51a
Iliyana Penova <110258890+iliyana523@users.noreply.github.com>
2023-06-11 22:48:15 +0300

    SoftUni
    Education

---
+++ DefiningClasses/RawData/Cargo.java
@@ -0,0 +1,11 @@
+package DefiningClasses.RawData;
+
+public class Cargo {
+    private int cargoWeight;
+    private String cargoType;
+
+    public Cargo(int cargoWeight, String cargoType) {
+        this.cargoWeight = cargoWeight;
+        this.cargoType = cargoType;
+    }
+}
